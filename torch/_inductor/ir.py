25bbc7864d7d940305121c12c6e41d23f516ec35
IvanKobzarev <ivan.kobzarev@gmail.com>
2026-04-01 13:26:17 -0700

    [inductor][comms] Optional use of process_group alloc_tensor for collectives
    ghstack-source-id: 7500800af1d9348126989e5987d86005d687a502
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/173386

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -4389,6 +4389,7 @@ class NonOwningLayout(Layout):
 
 class CommBufferType(Enum):
     SYMM_MEM = "symm_mem"
+    PG_ALLOC = "pg_alloc"
 
 
 class CommBufferLayout(FixedLayout):
--- torch/csrc/distributed/c10d/init.cpp
+++ torch/csrc/distributed/c10d/init.cpp
@@ -3145,6 +3145,10 @@ Arguments:
               py::arg("device"),
               py::call_guard<py::gil_scoped_release>())
           .def(
+              "is_initialized",
+              &::c10d::Backend::isInitialized,
+              py::call_guard<py::gil_scoped_release>())
+          .def(
               "allocate_tensor",
               [](::c10d::Backend& self,
                  long size,
