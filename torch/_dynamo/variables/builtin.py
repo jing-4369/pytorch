5ed640034c17a802893d16a101fb8d59d968cdad
Bob Ren <bobrenjc93@users.noreply.github.com>
2026-04-02 03:22:55 +0000

    Handle builtin container attrs in eager f-string formatting

--- torch/_dynamo/variables/builtin.py
+++ torch/_dynamo/variables/builtin.py
@@ -124,7 +124,10 @@ from .tensor import (
 from .user_defined import (
     MutableMappingVariable,
     UserDefinedDictVariable,
+    UserDefinedListVariable,
     UserDefinedObjectVariable,
+    UserDefinedSetVariable,
+    UserDefinedTupleVariable,
     UserDefinedVariable,
 )
 
@@ -1787,6 +1790,21 @@ class BuiltinVariable(VariableTracker):
     def call_default_format(
         self, tx: "InstructionTranslator", arg: VariableTracker
     ) -> VariableTracker | None:
+        if isinstance(arg, UserDefinedListVariable) and type(arg.value) is list:
+            arg = arg._list_vt
+        elif isinstance(arg, UserDefinedTupleVariable) and type(arg.value) is tuple:
+            arg = arg._tuple_vt
+        elif isinstance(arg, UserDefinedDictVariable) and type(arg.value) in (
+            dict,
+            OrderedDict,
+        ):
+            arg = arg._dict_vt
+        elif isinstance(arg, UserDefinedSetVariable) and type(arg.value) in (
+            set,
+            frozenset,
+        ):
+            arg = arg._set_vt
+
         if isinstance(
             arg,
             (
