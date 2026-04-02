a30cfd082d28b071108c11d0411f671ec217cd05
Animesh Jain <anijain@umich.edu>
2026-04-01 21:19:08 -0700

    [dynamo] Remove special casing for enum.Enum
    Removes the EnumVariable and let it fallback to UserDefinedObjectVariable. We have had many small issues with enums lately, and the fundamental reason is that enum is in python (not C), while Dynamo tries to short-circuit that path by implementing it as variable tracker, and this is a partial copy. This PR says - lets just trace into EnumType - the metaclass of enum.Enum.
    
    ghstack-source-id: 0af9c73ff391276f314a50e72155b0666bd4b5b8
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/179029

--- torch/_dynamo/variables/builtin.py
+++ torch/_dynamo/variables/builtin.py
@@ -92,7 +92,6 @@ from .constant import (
     CONSTANT_VARIABLE_FALSE,
     CONSTANT_VARIABLE_NONE,
     ConstantVariable,
-    EnumVariable,
     FakeIdVariable,
 )
 from .dicts import (
@@ -796,7 +795,6 @@ class BuiltinVariable(BaseBuiltinVariable):
                 # time benchmark - add_loop_eager.
                 result = [
                     ((ConstantVariable, ConstantVariable), compare_by_value),
-                    ((EnumVariable, EnumVariable), compare_by_value),
                 ]
 
                 op_var = BuiltinVariable(op)
@@ -3375,6 +3373,7 @@ class IterBuiltinVariable(BaseBuiltinVariable):
                     variables.NNModuleVariable,
                     variables.TensorVariable,
                     variables.TupleVariable,
+                    variables.UserDefinedClassVariable,
                     DictViewVariable,
                 ),
             )
